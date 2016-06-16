3c1db7cb4739b56ab49a74768282e85863d919d0
poddliza <poddliza@gmail.com>
2016-06-16 12:51:26 +0300

    first commit ventana-fixed

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*---like-button---*/
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=1126403354089470";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/*---slider---*/
+$('.bxslider').bxSlider({
+  auto: true,
+  autoControls: true
+});
